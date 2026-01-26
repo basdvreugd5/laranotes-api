d39e81854080791df5d9ab6b2d03dc447d744d9f
Bas de Vreugd <basdvreugd5@msn.com>
2026-01-26 16:05:59 +0100

    Add UserResource to explicitly define user API representation

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\NoteController;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    return new UserResource($request->user());
 })->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
